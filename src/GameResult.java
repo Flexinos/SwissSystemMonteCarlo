0133b6641d390b6458113756fdd5f02c46922d4e
Laurin <calathea@disroot.org>
2020-09-02 21:28:24 +0200

    change way game result is calculated
    result now depends on elo of both players.
    score formula rather arbitrary. draw formula completely arbitrary

--- src/GameResult.java
+++ src/GameResult.java
@@ -1,15 +1,30 @@
 import java.security.SecureRandom;
-import java.util.Map;
 
 public class GameResult {
     private static final SecureRandom random = new SecureRandom();
-    static Map<Integer, ResultOfGame> indexToResult = Map.of(0, ResultOfGame.WHITE_WIN, 1, ResultOfGame.DRAW, 2, ResultOfGame.BLACK_WIN);
+    //static Map<Integer, ResultOfGame> indexToResult = Map.of(0, ResultOfGame.WHITE_WIN, 1, ResultOfGame.DRAW, 2, ResultOfGame.BLACK_WIN);
 
     public static ResultOfGame randomResult(SimulatedPlayer p1, SimulatedPlayer p2) {
-        int x = random.nextInt(3);
-        return indexToResult.get(x);
+        double eloDiff = p1.getParticipant().getElo() - p2.getParticipant().getElo();
+        double p1Score = 1 / (1 + Math.pow(10, -eloDiff / 400));
+        double underdogScore;
+        if (p1Score > 0.5) {
+            underdogScore = 1 - p1Score;
+        } else {
+            underdogScore = p1Score;
+        }
+        double drawChance = underdogScore;
+        double p1WinChance = p1Score - drawChance * 0.5;
+        //double p2WinChance = 1 - p1Score - drawChance * 0.5;
+        double randomValue = random.nextDouble();
+        if (randomValue < p1WinChance) {
+            return ResultOfGame.WHITE_WIN;
+        } else if (randomValue < p1WinChance + drawChance) {
+            return ResultOfGame.DRAW;
+        } else {
+            return ResultOfGame.BLACK_WIN;
+        }
     }
-
 
     public enum ResultOfGame {
         WHITE_WIN, DRAW, BLACK_WIN
