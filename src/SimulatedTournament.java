6d3e43c9fe741e0ad6c48af150dfe7568221f65a
Laurin <calathea@disroot.org>
2020-09-03 21:01:22 +0200

    clean up code

--- src/SimulatedTournament.java
+++ src/SimulatedTournament.java
@@ -25,22 +25,17 @@ public class SimulatedTournament {
         simulatedPlayerArrayList.forEach(SimulatedPlayer::updateBuchholz);
         rankingByScoreThenEloArrayList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
         rankingByScoreThenTieBreakArrayList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByBUCHHOLZ));
-
         System.out.println("\n\nRanking after round: " + roundsFinished);
         rankingByScoreThenTieBreakArrayList.get(rankingByScoreThenTieBreakArrayList.size() - 1).getRanking().stream().map(player -> player.getParticipant().getName() + "\tScore: " + player.getScore() + "\tBucholz: " + player.getBuchholz() + "\tElo: " + player.getParticipant().getElo()).forEach(System.out::println);
     }
 
     public void simulateTournament() {
         rankingByScoreThenEloArrayList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
-
         System.out.println("Starting rank: ");
         rankingByScoreThenEloArrayList.get(rankingByScoreThenEloArrayList.size() - 1).getRanking().stream().map(player -> player.getParticipant().getName() + "\tScore: " + player.getScore() + "\tElo: " + player.getParticipant().getElo()).forEach(System.out::println);
-        System.out.print("\n\n");
-
         while (roundsFinished < tournament.getTotalRounds()) {
             getNextRound();
         }
-
         rankingByScoreThenEloArrayList.get(roundsFinished - 1).getRanking().stream().map(player -> player.getParticipant().getName() + " " + player.getScore()).forEach(System.out::println);
     }
 }
