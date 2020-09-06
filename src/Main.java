b3dd414bd830d4df112cfa09df447d8b7eb43e5a
Laurin <calathea@disroot.org>
2020-09-06 13:14:55 +0200

    rework Main.java code
    now does 100k runs. runtime timer starts at very beginning

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
-import java.security.SecureRandom;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static final int numberOfParticipants = 200;
-    public static final int numberOfRounds = 9;
+    public static final int numberOfParticipants = 300;
+    public static final int numberOfRounds = 11;
 
     public static void main(String[] args) {
-        final SecureRandom random = new SecureRandom();
+        long startTime = System.nanoTime();
+        final Random random = new Random();
         Tournament myTournament = new Tournament(numberOfRounds);
 
         List<Participant> participants = IntStream.range(0, numberOfParticipants).mapToObj(i -> new Participant("player " + i, 1000 + random.nextInt(1600))).collect(Collectors.toList());
         myTournament.addParticipants(participants);
 
-        SimulatedTournament mySimulatedTournament = new SimulatedTournament(myTournament);
+        for (int i = 0; i < 100000; i++) {
+            SimulatedTournament mySimulatedTournament = new SimulatedTournament(myTournament);
+            mySimulatedTournament.simulateTournament();
+            if (i % 1000 == 0) {
+                System.out.println(i);
+            }
+        }
 
-        long startTime = System.nanoTime();
-        mySimulatedTournament.simulateTournament();
         long endTime = System.nanoTime();
         long duration = (endTime - startTime) / 1000000;
         System.out.println(duration);
