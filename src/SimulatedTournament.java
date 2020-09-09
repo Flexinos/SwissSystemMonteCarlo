1373fd24babde1bf0fbc87f30168584d7e86f3cc
Laurin <calathea@disroot.org>
2020-09-09 23:34:25 +0200

    refactoring
    rename rankingByScoreThenEloArrayList to
    rankingByScoreThenEloList

--- src/SimulatedTournament.java
+++ src/SimulatedTournament.java
@@ -6,13 +6,13 @@ public class SimulatedTournament {
     private final List<SimulatedPlayer> simulatedPlayerArrayList;
     private int roundsFinished = 0;
     private final List<Round> roundArrayList;
-    private final List<Ranking> rankingByScoreThenEloArrayList;
+    private final List<Ranking> rankingByScoreThenEloList;
     private final List<Ranking> rankingByScoreThenTieBreakList;
 
     public SimulatedTournament(Tournament tournament) {
         this.tournament = tournament;
         this.roundArrayList = new ArrayList<>(tournament.getTotalRounds());
-        this.rankingByScoreThenEloArrayList = new ArrayList<>(tournament.getTotalRounds());
+        this.rankingByScoreThenEloList = new ArrayList<>(tournament.getTotalRounds());
         this.rankingByScoreThenTieBreakList = new ArrayList<>(tournament.getTotalRounds());
         this.simulatedPlayerArrayList = new ArrayList<>(tournament.getPlayerArrayList().size());
         tournament.getPlayerArrayList().stream().map(SimulatedPlayer::new).forEachOrdered(simulatedPlayerArrayList::add);
@@ -22,8 +22,8 @@ public class SimulatedTournament {
     }
 
     private void getNextRound() {
-        roundArrayList.add(new Round(rankingByScoreThenEloArrayList.get(roundsFinished++)));
-        rankingByScoreThenEloArrayList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
+        roundArrayList.add(new Round(rankingByScoreThenEloList.get(roundsFinished++)));
+        rankingByScoreThenEloList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
         //createRankingByScoreThenTieBreak();
         //System.out.println("\n\nRanking after round: " + roundsFinished);
         //rankingByScoreThenTieBreakArrayList.get(rankingByScoreThenTieBreakArrayList.size() - 1).getRanking().stream().map(player -> player.getParticipant().getName() + "\tScore: " + player.getScore() + "\tBucholz: " + player.getBuchholz() + "\tElo: " + player.getParticipant().getElo()).forEach(System.out::println);
@@ -35,7 +35,7 @@ public class SimulatedTournament {
     }
 
     public void simulateTournament() {
-        rankingByScoreThenEloArrayList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
+        rankingByScoreThenEloList.add(new Ranking(simulatedPlayerArrayList, Ranking.TypesOfRanking.ByELO));
         //System.out.println("Starting rank: ");
         //rankingByScoreThenEloArrayList.get(rankingByScoreThenEloArrayList.size() - 1).getRanking().stream().map(player -> player.getParticipant().getName() + "\tScore: " + player.getScore() + "\tElo: " + player.getParticipant().getElo()).forEach(System.out::println);
         while (roundsFinished < tournament.getTotalRounds()) {
