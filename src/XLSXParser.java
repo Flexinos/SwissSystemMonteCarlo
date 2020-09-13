5949d6389378edfa2e2d6b625c8a6584d83888b2
Daniel Rainer <rainer.daniel@gmx.at>
2020-09-14 00:15:38 +0200

    Start implementing data getter and parser
    The XLSXParser class should contin methods which take a link and
    return the relevant data in a usable format.
    
    Currently the class is just printing out the data
    for demonstration purposes.

---
+++ src/XLSXParser.java
@@ -0,0 +1,37 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+
+public class XLSXParser {
+    public static void main(String[] args) throws IOException {
+        getPairings("https://chess-results.com/tnr534098.aspx?lan=0&zeilen=0&art=2&rd=7&turdet=YES%20&flag=NO&prt=4&excel=2010");
+    }
+
+    public static void getPairings(String link) throws IOException {
+        XSSFSheet worksheet = getWorksheet(link);
+        for (Iterator<Row> rowIterator = worksheet.rowIterator(); rowIterator.hasNext(); ) {
+            Row row = rowIterator.next();
+            for (Iterator<Cell> cellIterator = row.cellIterator(); cellIterator.hasNext(); ) {
+                Cell cell = cellIterator.next();
+                if (cell.getCellType().equals(CellType.STRING)) {
+                    System.out.print(cell.getStringCellValue() + ",");
+                } else if (cell.getCellType().equals(CellType.NUMERIC)) {
+                    System.out.print(cell.getNumericCellValue() + ",");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void getRanking(String link){}
+
+    private static XSSFSheet getWorksheet(String link) throws IOException {
+        return new XSSFWorkbook(new URL(link).openStream()).getSheetAt(0);
+    }
+}
