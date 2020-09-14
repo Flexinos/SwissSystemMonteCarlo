176e2fdfcf2a5646b49df5f91e0acc98b3bdd967
Laurin <calathea@disroot.org>
2020-09-14 18:58:54 +0200

    add second constructor and more tiebreak class variables

--- src/SimulatedPlayer.java
+++ src/SimulatedPlayer.java
@@ -5,8 +5,10 @@ public class SimulatedPlayer {
     private final Participant participant;
     private SimulatedTournament simulatedTournament = null;
     private double score;
-    private double buchholz;
-    private final List<SimulatedPlayer> pastOpponents; //todo optimize
+    private final List<SimulatedPlayer> pastOpponents;
+    private double tieBreak1;
+    private double tieBreak2;
+    private double tieBreak3;
     private boolean receivedBye = false;
     private int colorDifference = 0;
 
@@ -17,12 +19,34 @@ public class SimulatedPlayer {
 
     public SimulatedPlayer(Participant participant, SimulatedTournament simulatedTournament) {
         this.participant = participant;
+        this.score = participant.getScore();
+        this.tieBreak1 = participant.getTieBreak1();
+        this.tieBreak2 = participant.getTieBreak2();
+        this.tieBreak3 = participant.getTieBreak3();
         this.simulatedTournament = simulatedTournament;
         this.pastOpponents = new ArrayList<>(Main.numberOfRounds);
     }
 
     public double getScore() {
         return score;
+    }
+
+    public static int compareToByTieBreak(SimulatedPlayer p1, SimulatedPlayer p2) {
+        if (p1.getScore() < p2.getScore()) {
+            return 1;
+        } else if (p1.getScore() > p2.getScore()) {
+            return -1;
+        } else {
+            return Double.compare(p2.getTieBreak1(), p1.getTieBreak1());
+        }
+    }
+
+    public double getTieBreak1() {
+        return tieBreak1;
+    }
+
+    public double getTieBreak2() {
+        return tieBreak2;
     }
 
     public void addGame(SimulatedPlayer opponent, double result) {
@@ -55,10 +79,6 @@ public class SimulatedPlayer {
         return simulatedTournament;
     }
 
-    public double getBuchholz() {
-        return buchholz;
-    }
-
     public List<SimulatedPlayer> getPastOpponents() {
         return pastOpponents;
     }
@@ -75,20 +95,14 @@ public class SimulatedPlayer {
         this.receivedBye = receivedBye;
     }
 
-    public void updateBuchholz() {
-        buchholz = 0;
-        for (SimulatedPlayer opponent : pastOpponents) {
-            buchholz += opponent.getScore();
-        }
+    public double getTieBreak3() {
+        return tieBreak3;
     }
 
-    public static int compareToByTieBreak(SimulatedPlayer p1, SimulatedPlayer p2) {
-        if (p1.getScore() < p2.getScore()) {
-            return 1;
-        } else if (p1.getScore() > p2.getScore()) {
-            return -1;
-        } else {
-            return Double.compare(p2.getBuchholz(), p1.getBuchholz());
+    public void updateTieBreaks() {
+        tieBreak1 = 0;
+        for (SimulatedPlayer opponent : pastOpponents) {
+            tieBreak1 += opponent.getScore();
         }
     }
 }
