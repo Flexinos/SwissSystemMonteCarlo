8bbf79c1dc72a8f2aada584474948a0eab6f8c43
Laurin <calathea@disroot.org>
2020-09-14 18:32:27 +0200

    rewrite Participant class to take more information
    Participant can now among other things also take current points as
    constructor argument. This is useful when simulating last round of a
    tournament and each SimulatedTournament needs to know how many points
    each player has.

--- src/Participant.java
+++ src/Participant.java
@@ -1,28 +1,32 @@
 public class Participant {
+    private final String title;
     private final String name;
+    private final String country;
+    private final double points;
     private final int elo;
+    private final double tieBreak1;
+    private final double tieBreak2;
+    private final double tieBreak3;
+    private final String type;
+    private final boolean isFemale;
     private int startingRank;
     private int numberOfTopThreeFinishes;
 
     public Participant(String name, int elo) {
+        this(name, "", "", elo, 0, 0, 0, 0, "", false);
+    }
+
+    public Participant(String name, String title, String country, int elo, double points, double tieBreak1, double tieBreak2, double tieBreak3, String type, boolean isFemale) {
         this.name = name;
+        this.title = title;
+        this.country = country;
         this.elo = elo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getElo() {
-        return elo;
-    }
-
-    public int getNumberOfTopThreeFinishes() {
-        return numberOfTopThreeFinishes;
-    }
-
-    public void setNumberOfTopThreeFinishes(int numberOfTopThreeFinishes) {
-        this.numberOfTopThreeFinishes = numberOfTopThreeFinishes;
+        this.points = points;
+        this.tieBreak1 = tieBreak1;
+        this.tieBreak2 = tieBreak2;
+        this.tieBreak3 = tieBreak3;
+        this.type = type;
+        this.isFemale = isFemale;
     }
 
     public int getStartingRank() {
@@ -31,6 +35,54 @@ public class Participant {
 
     public void setStartingRank(int startingRank) {
         this.startingRank = startingRank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getElo() {
+        return elo;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public double getTieBreak1() {
+        return tieBreak1;
+    }
+
+    public double getTieBreak2() {
+        return tieBreak2;
+    }
+
+    public double getTieBreak3() {
+        return tieBreak3;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public int getNumberOfTopThreeFinishes() {
+        return numberOfTopThreeFinishes;
+    }
+
+    public void setNumberOfTopThreeFinishes(int numberOfTopThreeFinishes) {
+        this.numberOfTopThreeFinishes = numberOfTopThreeFinishes;
     }
 
     public int compareToByElo(Participant p2) {
