4d0e3e8e213e38705b7878ea6d968c1bd63d45c1
Daniel Rainer <rainer.daniel@gmx.at>
2020-09-19 19:32:16 +0200

    Faster version of pgn parser
    Uses only sed.

--- parse_pgn_sed.sh
+++ parse_pgn_sed.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+#remove \r from regexes in first awk call if the file does not contain carriage returns
+
 if [ $# -ne 1 ]; then
 	echo 'Please give the pgn file as the only argument.'
 	exit
 fi
 
 sed -n '
-# remove carriage return
-s/\r//g
 # extract the results, i.e. 0, 1/2, or 1
-s/^\[Result "\(.\+\)\-.\+\]$/\1/
-# convert result 1 to 2
-s/^1$/2/p
-# convert result 1/2 to 1
-s/^1\/2$/1/p
-/^0$/p
+s/^\[Result "\(.\+\)\-.\+\]\r$/\1/p
 # print elo ratings
-s/^\[\(White\|Black\)Elo "\([0-9]\+\)"\]$/\2/p
-' $1 | sed -n '
+s/^\[\(White\|Black\)Elo "\([0-9]\+\)"\]\r$/\2/p
+' $1 |
+sed '
+# convert result 1 to 2
+s/^1$/2/
+# convert result 1/2 to 1
+s/^1\/2$/1/
+' |
+sed -n '
 # convert all sequences of lines consisting of result\nelo\nelo into result,elo,elo
 /^[012]$/ {
 	N
