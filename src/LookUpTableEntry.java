1ebcaba10f56d7f516cfcc0a5bdf9945c2f58711
Daniel Rainer <rainer.daniel@gmx.at>
2020-09-25 00:06:12 +0200

    Remove elo values from LookUpTableEntry.java
    Both player's elo values are indicated by the position
    in the List/array where the LookUpTableEntry
    is stored.

--- src/LookUpTableEntry.java
+++ src/LookUpTableEntry.java
@@ -1,11 +1,7 @@
 public class LookUpTableEntry {
-    public final int ELO_WHITE;
-    public final int ELO_BLACK;
     public final float[] PROBABILITIES;
 
     public LookUpTableEntry(String[] splitLine) {
-        ELO_WHITE = Integer.parseInt(splitLine[0]);
-        ELO_BLACK = Integer.parseInt(splitLine[1]);
         PROBABILITIES = new float[]{Float.parseFloat(splitLine[2]), Float.parseFloat(splitLine[3]), Float.parseFloat(splitLine[4])};
     }
 }
