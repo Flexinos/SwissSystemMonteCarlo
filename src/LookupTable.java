bc638ddc43d60af8e4dda056f278d70985ff40ed
Laurin <calathea@disroot.org>
2020-09-26 23:16:03 +0200

    remove unnecessary class LookupTableEntry.java
    Class was only used to store a single array. Same functionality now in LookupTable.java
    with fewer lines of code.

--- src/LookupTable.java
+++ src/LookupTable.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class LookupTable {
     // ATTENTION: The lookupTableFile's contents must match the variables LOWEST_ELO and HIGHEST_ELO.
-    private static final String lookupTableFile = "C:\\Users\\Laurin\\Desktop\\lookuptable1.txt";
+    private static final String lookupTableFile = "C:\\Users\\Laurin\\Desktop\\lookup_table.txt";
     private static final int LOWEST_ELO = 800;
     private static final int HIGHEST_ELO = 2999;
     private static final int ELO_RANGE = HIGHEST_ELO - LOWEST_ELO + 1;
@@ -21,7 +21,8 @@ public class LookupTable {
         try (FileInputStream inputStream = new FileInputStream(lookupTableFile); Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8)) {
             for (int row = 0; row < ELO_RANGE; ++row) {
                 for (int column = 0; column < ELO_RANGE; ++column) {
-                    lookupTable[row][column] = new LookupTableEntry(sc.nextLine().split(" ")).PROBABILITIES;
+                    String[] splitLine = sc.nextLine().split(" ");
+                    lookupTable[row][column] = new float[]{Float.parseFloat(splitLine[2]), Float.parseFloat(splitLine[3]), Float.parseFloat(splitLine[4])};
                 }
             }
         } catch (IOException | NoSuchElementException e) {
--- src/LookupTableEntry.java
+++
@@ -1,7 +0,0 @@
-public class LookupTableEntry {
-    public final float[] PROBABILITIES;
-
-    public LookupTableEntry(String[] splitLine) {
-        PROBABILITIES = new float[]{Float.parseFloat(splitLine[2]), Float.parseFloat(splitLine[3]), Float.parseFloat(splitLine[4])};
-    }
-}
