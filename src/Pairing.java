b4c58bd86ab18318e7e5b950606a31f88ebc585a
Laurin <calathea@disroot.org>
2020-09-26 23:08:39 +0200

    pull GameResult.java class into Pairing.java
    GameResult only contained one method that was only used by Pairing, which made the class
    itself completely redundant

--- src/Pairing.java
+++ src/Pairing.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class Pairing {
     private final SimulatedPlayer player1;
     private final SimulatedPlayer player2;
-    private GameResult.ResultOfGame result;
+    private static final Random random = new Random();
+    private ResultOfGame result;
 
     public Pairing(PossiblePairing pairing) {
         this.player1 = pairing.getPlayer1();
@@ -11,7 +14,7 @@ public class Pairing {
     public Pairing(PossiblePairing pairing, boolean includesBye) {
         this(pairing);
         if (includesBye) {
-            result = GameResult.ResultOfGame.BYE;
+            result = ResultOfGame.BYE;
             player1.addGame(player2, 1);
         }
     }
@@ -33,11 +36,23 @@ public class Pairing {
         return player2;
     }
 
+    private static ResultOfGame randomResultLookUp(SimulatedPlayer whitePlayer, SimulatedPlayer blackPlayer) {
+        float randomValue = random.nextFloat();
+        float[] probabilitiesArray = LookupTable.getProbabilities(whitePlayer.getElo(), blackPlayer.getElo());
+        if (randomValue < probabilitiesArray[0]) {
+            return ResultOfGame.BLACK_WIN;
+        } else if (randomValue < probabilitiesArray[0] + probabilitiesArray[1]) {
+            return ResultOfGame.DRAW;
+        } else {
+            return ResultOfGame.WHITE_WIN;
+        }
+    }
+
     public void simulateResult() {
         if (result != null) {
             return;
         }
-        result = GameResult.randomResultLookUp(player1, player2);
+        result = randomResultLookUp(player1, player2);
         switch (result) {
             case WHITE_WIN -> {
                 player1.addGame(player2, 1, true);
@@ -52,19 +67,22 @@ public class Pairing {
                 player2.addGame(player1, 0.5, false);
             }
         }
-        //System.out.println(player1.getParticipant().getName() + "played against " + player2.getParticipant().getName());
     }
 
     @Override
     public String toString() {
-        if (result.equals(GameResult.ResultOfGame.WHITE_WIN)) {
+        if (result.equals(ResultOfGame.WHITE_WIN)) {
             return player1 + "1-0 " + player2;
-        } else if (result.equals(GameResult.ResultOfGame.DRAW)) {
+        } else if (result.equals(ResultOfGame.DRAW)) {
             return player1 + "0.5-0.5 " + player2;
-        } else if (result.equals(GameResult.ResultOfGame.BLACK_WIN)) {
+        } else if (result.equals(ResultOfGame.BLACK_WIN)) {
             return player1 + "0-1 " + player2;
         } else {
             return player1 + " 1 " + "spielfrei";
         }
+    }
+
+    private enum ResultOfGame {
+        WHITE_WIN, DRAW, BLACK_WIN, BYE
     }
 }
