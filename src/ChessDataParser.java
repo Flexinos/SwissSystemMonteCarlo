fb00effa49a04adce1274188f2ec5a2c317d5d39
Daniel Rainer <rainer.daniel@gmx.at>
2020-09-28 18:09:12 +0200

    Create static nested class
    ChessDataParser should only contain
    the public interface and the methods
    required by both Pairing and Ranking
    parsers.
    
    The new static nested class PairingUtilities
    should contain all pairing-specific code.

--- src/ChessDataParser.java
+++ src/ChessDataParser.java
@@ -16,56 +16,26 @@ public class ChessDataParser {
         assert test1.equals(test3);
     }
 
-    private enum ChessDataType { PAIRING, RANKING}
-
     // inputLink should contain a valid link to a tournament on chess-results.com,
     // from a page which shows the desired round.
     public static List<int[]> getPairings(String inputLink) {
-        return getPairings(buildLinkFromString(inputLink, ChessDataType.PAIRING));
+        return PairingUtilities.getPairings(buildLinkFromString(inputLink, ChessDataType.PAIRING));
     }
 
     // This method allows using any link from the tournament, the round in the link is ignored.
     // The desired round is set via the method's argument.
     public static List<int[]> getPairings(String inputLink, int round) {
-        return getPairings(buildLinkFromValues(getTournamentNumber(inputLink), round, ChessDataType.PAIRING));
+        return PairingUtilities.getPairings(buildLinkFromValues(getTournamentNumber(inputLink), round, ChessDataType.PAIRING));
     }
 
     public static List<int[]> getPairings(int tournamentNumber, int round) {
-        return getPairings(buildLinkFromValues(tournamentNumber, round, ChessDataType.PAIRING));
+        return PairingUtilities.getPairings(buildLinkFromValues(tournamentNumber, round, ChessDataType.PAIRING));
     }
+
+    private enum ChessDataType { PAIRING, RANKING}
 
     // Prevent instantiation
     private ChessDataParser(){}
-
-    private static List<int[]> getPairings(URL link) {
-        Scanner scanner = getScanner(link);
-        List<int[]> pairings = new ArrayList<>();
-        boolean paringsStarted = false;
-        while (scanner.hasNextLine()) {
-            // Remove HTML tags and numerical character code points.
-            String line = scanner.nextLine()
-                    .replaceAll("<[^>]*>", "")
-                    .replaceAll("&#\\d*;", "");
-            if (!paringsStarted) {
-                // Find the start of the table.
-                if (line.startsWith("Br.;Nr.;Name;")) {
-                    // Parse pairings starting from the next line.
-                    paringsStarted = true;
-                }
-            } else {
-                // Stop parsing pairings on the first line which does not start with a digit.
-                if (!line.matches("^\\d.*")) {
-                    break;
-                }
-                int[] pairing = parsePairingLine(line);
-                // "nicht ausgelost" pairings return null and should not be added to the list.
-                if (pairing != null) {
-                    pairings.add(pairing);
-                }
-            }
-        }
-        return pairings;
-    }
 
     private static URL buildLinkFromString(String inputLink, ChessDataType type) {
         int tournamentNumber = getTournamentNumber(inputLink);
@@ -124,37 +94,69 @@ public class ChessDataParser {
         }
     }
 
-    private static int[] parsePairingLine(String line) {
-        String[] lineEntries = line.split("[;]");
-        // White's starting rank and title are not separated.
-        // Only using the digits in the string hopefully solves this issue.
-        int whiteStartingRank = Integer.parseInt(lineEntries[1].replaceAll("[^0-9]+", ""));
-        String lastEntry = lineEntries[lineEntries.length - 1];
-        int blackStartingRank;
-        if (lastEntry.equals("spielfrei")) {
-            blackStartingRank = 0;
-        } else if (lastEntry.equals("nicht ausgelost")) {
-            // Pairing will not be returned.
-            return null;
-        } else {
-            // In some cases separators are missing.
-            // Only using the digits in the string hopefully solves this issue.
-            String lastEntryNumbersOnly = lastEntry.replaceAll("[^0-9]+", "");
-            String blackStartingRankString;
-            // The input data has no separator between black elo and black starting rank,
-            // so this is necessary.
-            if (lastEntryNumbersOnly.startsWith("0")) {
-                // Elo is zero.
-                blackStartingRankString = lastEntryNumbersOnly.substring(1);
-            } else if (lastEntryNumbersOnly.matches("^[12]\\d+")) {
-                // Elo has four digits.
-                blackStartingRankString = lastEntryNumbersOnly.substring(4);
-            } else {
-                // Elo has three digits.
-                blackStartingRankString = lastEntryNumbersOnly.substring(3);
+    private static class PairingUtilities {
+        private static List<int[]> getPairings(URL link) {
+            Scanner scanner = getScanner(link);
+            List<int[]> pairings = new ArrayList<>();
+            boolean paringsStarted = false;
+            while (scanner.hasNextLine()) {
+                // Remove HTML tags and numerical character code points.
+                String line = scanner.nextLine()
+                        .replaceAll("<[^>]*>", "")
+                        .replaceAll("&#\\d*;", "");
+                if (!paringsStarted) {
+                    // Find the start of the table.
+                    if (line.startsWith("Br.;Nr.;Name;")) {
+                        // Parse pairings starting from the next line.
+                        paringsStarted = true;
+                    }
+                } else {
+                    // Stop parsing pairings on the first line which does not start with a digit.
+                    if (!line.matches("^\\d.*")) {
+                        break;
+                    }
+                    int[] pairing = parsePairingLine(line);
+                    // "nicht ausgelost" pairings return null and should not be added to the list.
+                    if (pairing != null) {
+                        pairings.add(pairing);
+                    }
+                }
             }
-            blackStartingRank = Integer.parseInt(blackStartingRankString);
+            return pairings;
         }
-        return new int[]{whiteStartingRank, blackStartingRank};
+
+        private static int[] parsePairingLine(String line) {
+            String[] lineEntries = line.split("[;]");
+            // White's starting rank and title are not separated.
+            // Only using the digits in the string hopefully solves this issue.
+            int whiteStartingRank = Integer.parseInt(lineEntries[1].replaceAll("[^0-9]+", ""));
+            String lastEntry = lineEntries[lineEntries.length - 1];
+            int blackStartingRank;
+            if (lastEntry.equals("spielfrei")) {
+                blackStartingRank = 0;
+            } else if (lastEntry.equals("nicht ausgelost")) {
+                // Pairing will not be returned.
+                return null;
+            } else {
+                // In some cases separators are missing.
+                // Only using the digits in the string hopefully solves this issue.
+                String lastEntryNumbersOnly = lastEntry.replaceAll("[^0-9]+", "");
+                String blackStartingRankString;
+                // The input data has no separator between black elo and black starting rank,
+                // so this is necessary.
+                if (lastEntryNumbersOnly.startsWith("0")) {
+                    // Elo is zero.
+                    blackStartingRankString = lastEntryNumbersOnly.substring(1);
+                } else if (lastEntryNumbersOnly.matches("^[12]\\d+")) {
+                    // Elo has four digits.
+                    blackStartingRankString = lastEntryNumbersOnly.substring(4);
+                } else {
+                    // Elo has three digits.
+                    blackStartingRankString = lastEntryNumbersOnly.substring(3);
+                }
+                blackStartingRank = Integer.parseInt(blackStartingRankString);
+            }
+            return new int[]{whiteStartingRank, blackStartingRank};
+        }
     }
 }
