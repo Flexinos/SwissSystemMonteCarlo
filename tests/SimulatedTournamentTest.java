50839cdca8f5c507920c8d5ab4964c2114085053
laurin <calathea@disroot.org>
2020-11-23 01:24:54 +0100

    Fix unit tests

--- tests/SimulatedTournamentTest.java
+++ tests/SimulatedTournamentTest.java
@@ -22,17 +22,17 @@ class SimulatedTournamentTest {
     @Test
     void createPlayerDataStringTRF_TestJavafoSample() {
         assertEquals(SimulatedTournament.createPlayerDataStringTRF(1, p1),
-                "001    1 m  g Mirzoev Azer                      2527 AZE  1234567890 1978        0.0    1");
+                "001    1 m  g Mirzoev Azer                      2527 AZE     1234567 1978        0.0    1");
         assertEquals(SimulatedTournament.createPlayerDataStringTRF(2, p2),
-                "001    2 m  m Argandona Riveiro Inigo           2408 ESP  1234567890 1978        0.0    2");
+                "001    2 m  m Argandona Riveiro Inigo           2408 ESP     1234567 1978        0.0    2");
     }
 
     @Test
     void createOpponentsStringTRF_TestJavafoSample() {
-        p1.addGame(p2, '1', true);
-        p2.addGame(p1, '0', false);
-        assertEquals(SimulatedTournament.createOpponentsStringTRF(p1.getGameList()), "     2 w 1\n");
-        assertEquals(SimulatedTournament.createOpponentsStringTRF(p2.getGameList()), "     1 b 0\n");
+        p1.addGame(p2.getStartingRank(), '1', 'w');
+        p2.addGame(p1.getStartingRank(), '0', 'b');
+        assertEquals(SimulatedTournament.createOpponentsStringTRF(p1.getGameList()), "     2 w 1");
+        assertEquals(SimulatedTournament.createOpponentsStringTRF(p2.getGameList()), "     1 b 0");
     }
     /*
     @Test
