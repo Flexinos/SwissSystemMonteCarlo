67a017ae74efbd28ad0f355e29d0ab395652ea8a
laurin <calathea@disroot.org>
2020-11-23 01:18:29 +0100

    Remove unused functions

--- src/OpponentWrapper.java
+++ src/OpponentWrapper.java
@@ -3,24 +3,11 @@ public class OpponentWrapper {
     private Character result;
     private Character color;
 
-    public OpponentWrapper(int opponentStartingRank) {
-        this.opponentStartingRank = opponentStartingRank;
-    }
-
     public OpponentWrapper(int opponentStartingRank, Character result, Character color) {
         this.opponentStartingRank = opponentStartingRank;
         this.result = result;
         this.color = color;
     }
-
-    public void setResult(Character result) {
-        this.result = result;
-    }
-
-    public void setColor(Character color) {
-        this.color = color;
-    }
-
     public int getOpponentStartingRank() {
         return opponentStartingRank;
     }
